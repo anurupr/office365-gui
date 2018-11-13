f86d1edb1f9b4892bd789bc490210271742fd398
Anurup Raveendran <anurupraveendran@gmail.com>
2018-11-13 15:18:49 +0000

    psh to pwsh

--- constants.js
+++ constants.js
@@ -2,5 +2,5 @@
 var os = require('os')
 var platform = os.platform()
 module.exports = {
-  POWERSHELL_COMMAND: platform == "linux" ? "psh" : "powershell"
+  POWERSHELL_COMMAND: platform == "linux" ? "pwsh" : "powershell"
 }
